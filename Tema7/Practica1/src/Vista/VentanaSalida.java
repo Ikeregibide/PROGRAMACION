6a268ca252ba3d2db6e023edce84916c3556a2bc
Ikeregibide <94176884+Ikeregibide@users.noreply.github.com>
2022-01-29 22:32:21 +0100

    Add files via upload

---
+++ Tema7/Practica1/src/Vista/VentanaSalida.java
@@ -0,0 +1,19 @@
+package Vista;
+
+import javax.swing.*;
+
+
+public class VentanaSalida {
+    private JPanel VentanaSalida;
+    private JLabel LTitulo;
+    private JTextArea TATexto;
+    private JButton BSalir;
+
+
+    public static void dameDatos(String nombre, String apellidos, String dni){
+
+    }
+
+
+    }
+
